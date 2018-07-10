38def7c1ace6a8afaba3f3005bece3843cf1eac9
Yechao <gxhn_yc@163.com>
2018-07-10 19:14:37 +0800

    springSecurity-基本配置

--- src/main/java/springmvc/test/config/SecurityConfig.java
+++ src/main/java/springmvc/test/config/SecurityConfig.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //开启WebSecurity组件功能，获得了SpringSecurityFilterChain组件
+
 public class SecurityConfig {
+	// 默认所有请求都需要登录，对于这个后台管理系统来说，可以不改
 	
 	@Bean
     public UserDetailsService userDetailsService() {
-        // 提供用户详情，用于检查登录的用户名和密码
+        // 提供用户详情，用于检查登录的用户名和密码,创建了两个用户，密码为加密后的‘123456’,一个账户对应一个会话
         InMemoryUserDetailsManager um = new InMemoryUserDetailsManager();
         um.createUser(User
                 .withUsername("zs")
